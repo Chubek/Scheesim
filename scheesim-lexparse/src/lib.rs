d3ff03c024a13f2d2aa4343689839c11cd0d7821
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-02-10 01:27:30 +0330

    wrote v1 of parser

--- scheesim-lexparse/src/lib.rs
+++ scheesim-lexparse/src/lib.rs
@@ -1,74 +1,44 @@
-pub enum Token {
-    Semicolon,
-    Dash,
-    Equal,
-    Period,
-    Dollar,
-    Asterisk, 
-    Newline,                   
-    Character(char),
+use std::process::exit;
+
+pub enum ComponentMarker {
+    ACSweep,
+    DCSource,
+    Resistor,
+    Capacitor,
+    Inductor,
+    Transistor,
+    Diode,
 }
 
+macro_rules! error_out {
+    ($m:literal, $($f:ident),*) => {
+        {
+            let string = format!($m, $($f),*);
+            eprintln!("\x1b[1;31mError:\x1b[0m {}", string);
+            exit(1);
+        }
 
-pub struct TokenGarble {
-    tokens: Vec<Token>,
-    cursor: usize,
+    };
 }
 
-pub enum ResultAdvance {
-    Advanced,
-    EndReached,
-}
-
-impl TokenGarble {
-    pub fn advance(&mut self) -> Result<ResultAdvance, ()> {
-        self.cursor += 1;
-        
-        match self.tokens.len() == self.cursor {
-            true => Ok(ResultAdvance::Advanced),
-            false => Ok(ResultAdvance::EndReached),
+impl ComponentMarker {
+    pub fn from(s: &str, line_num: usize) -> Self {
+        match s.to_lowercase().as_str() {
+            ".acsweep" => Self::ACSweep,
+            ".dcsource" => Self::DCSource,
+            ".resistor" => Self::Resistor,
+            ".capacitor" => Self::Capacitor,
+            ".inductor" => Self::Inductor,
+            ".transistor" => Self::Transistor,
+            ".diode" => Self::Diode,
+            _ => error_out!("Unknown element: {} at line {}", s, line_num),
         }
     }
-
-    pub fn get_at_cursor(&self) -> Token {
-        *self.tokens.get(self.cursor).unwrap()
-    }
-
-    pub fn get_at_cursor_and_advance(&mut self)
-
-    pub fn peek(&self, offset: usize) -> Result<Token, ()> {
-        match self.tokens.get(self.cursor + offset) {
-            Some(t) => Ok(*t),
-            None => Err(()),
-        }
-    }
-
- 
-}
-
-pub struct Argument {
-    flag: String,
-    value: Option<String>,
-    asterisk: bool,
-}
-
-impl Argument {
-    fn from(tokens: &mut TokenGarble) -> Self {
-        match tokens.get_at_cursor()
-    }
-}
-
-pub enum Lexeme {
-    Name(String),
-    Arg(Argument),
-    ProfileMarker(String),
-    RawElement(String),
-    ProbeMarker,
 }
 
 pub enum Unit {
-    Quettam(f64),
-    Robba(f64),
+    Quetta(f64),
+    Ronna(f64),
     Yotta(f64),
     Zetta(f64),
     Exa(f64),
@@ -79,11 +49,13 @@ pub enum Unit {
     Kilo(f64),
     Hecto(f64),
     Deca(f64),
-    None(f64),
+    One(f64),
     Deci(f64),
     Centi(f64),
     Milli(f64),
     Micro(f64),
+    Nano(f64),
+    Pico(f64),
     Femto(f64),
     Atto(f64),
     Zepto(f64),
@@ -92,33 +64,240 @@ pub enum Unit {
     Quecto(f64),
 }
 
-pub enum DCType {
-    Current,
-    Voltage,
-    CurrentControlsCurrent,
-    CurrentControlsVoltage,
-    VoltageControlsCurrent,
-    VoltageControlsVoltage,
+impl Unit {
+    pub fn from(s: &str, line_number: usize) -> Self {
+        let (digits, unit) = {
+            let mut digits = String::new();
+            let mut unit = String::new();
+
+            for (i, ch) in s.char_indices() {
+                if ch.is_numeric() || ch == '.' || ch == 'e' {
+                    digits.push(ch);
+                } else if vec![
+                    'Q', 'R', 'R', 'Y', 'Z', 'E', 'P', 'T', 'G', 'M', 'k', 'h', 'd', 'a', 'c', 'm',
+                    'u', 'n', 'p', 'f', 'z', 'y', 'r', 'q',
+                ]
+                .contains(&ch)
+                {
+                    unit.push(ch);
+                } else {
+                    error_out!("Error in SI unit '{}' in line {}", s, line_number);
+                }
+            }
+
+            (digits, unit)
+        };
+
+        let unit_parsed = digits.parse::<f64>().unwrap();
+
+        match unit.as_str() {
+            "Q" => Self::Quetta(unit_parsed),
+            "R" => Self::Ronna(unit_parsed),
+            "Y" => Self::Yotta(unit_parsed),
+            "Z" => Self::Zetta(unit_parsed),
+            "E" => Self::Exa(unit_parsed),
+            "P" => Self::Peta(unit_parsed),
+            "T" => Self::Tera(unit_parsed),
+            "G" => Self::Giga(unit_parsed),
+            "M" => Self::Mega(unit_parsed),
+            "k" => Self::Kilo(unit_parsed),
+            "h" => Self::Hecto(unit_parsed),
+            "d" => Self::Deci(unit_parsed),
+            "a" => Self::Atto(unit_parsed),
+            "da" => Self::Deca(unit_parsed),
+            "c" => Self::Centi(unit_parsed),
+            "m" => Self::Milli(unit_parsed),
+            "u" => Self::Micro(unit_parsed),
+            "n" => Self::Nano(unit_parsed),
+            "p" => Self::Pico(unit_parsed),
+            "f" => Self::Femto(unit_parsed),
+            "z" => Self::Zepto(unit_parsed),
+            "y" => Self::Yocto(unit_parsed),
+            "r" => Self::Ronto(unit_parsed),
+            "q" => Self::Quecto(unit_parsed),
+            _ => Self::One(unit_parsed),
+        }
+    }
+
+    pub fn get_corresponding_value(&self) -> f64 {
+        match self {
+            Unit::Quetta(u) => u * 1e30,
+            Unit::Ronna(u) => u * 1e27,
+            Unit::Yotta(u) => u * 1e24,
+            Unit::Zetta(u) => u * 1e21,
+            Unit::Exa(u) => u * 1e18,
+            Unit::Peta(u) => u * 1e15,
+            Unit::Tera(u) => u * 1e12,
+            Unit::Giga(u) => u * 1e9,
+            Unit::Mega(u) => u * 1e6,
+            Unit::Kilo(u) => u * 1e3,
+            Unit::Hecto(u) => u * 1e2,
+            Unit::Deca(u) => u * 1e1,
+            Unit::One(u) => u * 1e0,
+            Unit::Deci(u) => u * 1e-1,
+            Unit::Centi(u) => u * 1e-2,
+            Unit::Milli(u) => u * 1e-3,
+            Unit::Micro(u) => u * 1e-6,
+            Unit::Nano(u) => u * 1e-9,
+            Unit::Pico(u) => u * 1e-12,
+            Unit::Femto(u) => u * 1e-15,
+            Unit::Atto(u) => u * 1e-18,
+            Unit::Zepto(u) => u * 1e-21,
+            Unit::Yocto(u) => u * 1e-24,
+            Unit::Ronto(u) => u * 1e-27,
+            Unit::Quecto(u) => u * 1e-30,
+        }
+    }
 }
 
-pub enum Bias {
-    NPN,
-    PNP,
-    NP,
-    PN,
-    N,
-    P,
+pub enum Argument {
+    Author(String),
+    Date(String),
+    In(String),
+    Out(String),
+    Base(String),
+    Voltage(Unit),
+    Power(Unit),
+    Current(Unit),
+    Inductance(Unit),
+    Capacitance(Unit),
+    Resistance(Unit),
+    Frequency(String),
+    Dynamic,
+    Nonlinear,
 }
 
+impl Argument {
+    pub fn from(s: &str, line_number: usize) -> Self {
+        let mut split_on_equal = s.split('=');
 
+        let name = split_on_equal.next().unwrap().trim();
 
-pub enum Component {
-    ACSweep(f64, f64),
-    DCSource(Option<f64>, Option<f64>, DCType),
-    Resistor(f64, bool),
-    Capacitor(f64, bool, bool),
-    Inductor(f64, bool, bool),
-    Transistor(f64, Bias, bool),
-    Diode(f64, Bias),
+        match name.to_lowercase().as_str() {
+            "-dynamic" => Self::Dynamic,
+            "-nonlinear" => Self::Nonlinear,
+            _ => match split_on_equal.next() {
+                Some(v) => {
+                    let value = v.trim().to_string();
+
+                    match name.to_lowercase().as_str() {
+                        "-author" => Self::Author(value),
+                        "-date" => Self::Date(value),
+                        "-in" => Self::In(value),
+                        "-base" => Self::Base(value),
+                        "-out" => Self::Out(value),
+
+                        _ => {
+                            let value_unit = Unit::from(&value, line_number);
+
+                            match name.to_lowercase().as_str() {
+                                "-voltage" => Self::Voltage(value_unit),
+                                "-current" => Self::Current(value_unit),
+                                "-power" => Self::Power(value_unit),
+                                "-inducance" => Self::Inductance(value_unit),
+                                "-capacitance" => Self::Capacitance(value_unit),
+                                "-resistance" => Self::Resistance(value_unit),
+                                "-frequency" => Self::Voltage(value_unit),
+                                _ => error_out!(
+                                    "Wrong type of argument: '{}' in line {}",
+                                    s,
+                                    line_number
+                                ),
+                            }
+                        }
+                    }
+                }
+                None => error_out!(
+                    "Wrong argument given: '{}' in line {}. Must have value sperated by =",
+                    s,
+                    line_number
+                ),
+            },
+        }
+    }
 }
 
+pub enum Lexeme {
+    NetlistName(String),
+    Component(ComponentMarker),
+    NodeName(String),
+    ProfileName(String),
+    Arg(Argument),
+    Pobe,
+    EndMarker,
+    Comment,
+}
+
+impl Lexeme {
+    pub fn from(s: &str, line_number: usize) -> Self {
+        let mut char_iter = s.chars();
+
+        match char_iter.next().unwrap() {
+            ';' => {
+                if s.chars().filter(|ch| *ch == ';').count() > 3 {
+                    error_out!(
+                        "Wrong lexeme '{}' at line {}: Cannot contain more than 3 semicolons.",
+                        s,
+                        line_number
+                    );
+                }
+
+                let res = match char_iter.next() {
+                    Some(ch) => match ch {
+                        ';' => {
+                            let res = match char_iter.next() {
+                                    Some(ch) => match ch {
+                                        ';' => Self::ProfileName(s.replace(";;;", "")),
+                                        _ => Self::NodeName(s.replace(";;", "")),
+                                    },
+                                    None => error_out!("Wrong token '{}' at line {}, semicolon marker cannot be left empty", ch, line_number),
+                                };
+
+                            res
+                        }
+                        _ => Self::NetlistName(s.replace(";", "")),
+                    },
+                    None => Self::EndMarker,
+                };
+                res
+            }
+            '-' => Self::Arg(Argument::from(s, line_number)),
+            '$' => match s.to_lowercase().contains("probe") {
+                true => Self::Pobe,
+                false => error_out!("Lexeme '{}' in line {}: it must be $PROBE", s, line_number),
+            },
+            '/' => Self::Comment,
+            _ => error_out!(
+                "Problem with lexeme '{}' in line {}: it's not a valid lexeme for Scheesim Netlist",
+                s,
+                line_number
+            ),
+        }
+    }
+}
+
+pub struct LexemeLine(Vec<Lexeme>);
+
+impl LexemeLine {
+    pub fn from(s: &str, line_number: usize) -> Self {
+        Self(
+            s.split_whitespace()
+                .map(|s| Lexeme::from(s, line_number))
+                .collect::<Vec<Lexeme>>(),
+        )
+    }
+}
+
+pub struct Netlist(Vec<LexemeLine>);
+
+impl Netlist {
+    pub fn from(netlist: &str) -> Self {
+        Self(
+            netlist
+                .lines()
+                .enumerate()
+                .map(|(i, s)| LexemeLine::from(s, i + 1))
+                .collect(),
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-use scheesim_lexparse::ScheesimSchema;
-use std::fs::File;
-use std::io::read_to_string;
-
 fn main() {
-    let ss = read_to_string(File::open("examples/test.scheenet").unwrap()).unwrap();
-
-    let schema = ScheesimSchema::from(ss.trim());
-
-    println!("{:?}", schema);
+   
 }
